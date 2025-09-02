0eb8476901d3e2a605fe18352906cc8f2f327ee3
Your Name <YourName@gmail.com>
2025-09-02 16:46:17 -0500

    first commit

---
+++ estructuras..py
@@ -0,0 +1,52 @@
+# Autor
+## El marcador for refleja...
+# declracion de variables
+
+
+xp = 0
+xf = 21
+
+
+#el  cliente ingresa un valor de compra y el programa devuelve un descuento
+
+#Funciones
+# disenar una encuesta para 6 users, 
+# Nombre
+# carrera
+# idea de proyecto
+
+
+def hola():
+       print("Hola mundo")
+
+def operacion():
+       for i in  (10,"casa",-3):
+            if(i == "casa"):
+                print(f"La palabra {i}")
+
+def multiplos(m):
+
+            print(f"hola {m} ")
+            boleano = "5 * 3"
+            print(type(boleano),boleano)
+
+def main():
+    #multiplos(5) 
+    #hola()
+    operacion()
+
+
+
+if __name__ == "__main__":
+    main()
+
+  
+
+
+    
+
+
+
+
+
+
